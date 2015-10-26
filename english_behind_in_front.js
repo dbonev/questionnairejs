d2a7e40331fd0c69449d266633e408d44794c08e
Dimitar Bonev <dimitar.bonev@gmail.com>
2015-10-26 10:20:09 +0200

    Fixes

--- english_behind_in_front.js
+++ english_behind_in_front.js
@@ -3,7 +3,7 @@ var test = require('./questionaire_module');
 var questions = [];
 
 var q = test.create_question("O |", "2");
-test.set_explanation("Choose behind, in front of, next to, between");
+test.set_explanation(q, "Choose behind, in front of, next to, between");
 test.set_multichoice(q, ["1) Behind", "2) Next to", "3) Between", "4) In Front of"]);
 questions.push(q);
 
