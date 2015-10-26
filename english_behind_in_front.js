fb458b9b804e8f65f837956bd5f0b07c2be7b23a
Dimitar Bonev <dimitar.bonev@gmail.com>
2015-10-26 10:12:42 +0200

    Added a new test behind / in front of etc.

---
+++ english_behind_in_front.js
@@ -0,0 +1,22 @@
+var test = require('./questionaire_module');
+
+var questions = [];
+
+var q = test.create_question("O |", "2");
+test.set_explanation("Choose behind, in front of, next to, between");
+test.set_multichoice(q, ["1) Behind", "2) Next to", "3) Between", "4) In Front of"]);
+questions.push(q);
+
+q = test.create_question("O | O", "3");
+test.set_multichoice(q, ["1) Behind", "2) Next to", "3) Between", "4) In Front of"]);
+questions.push(q);
+
+q = test.create_question("O", "1");
+test.set_multichoice(q, ["1) Behind", "2) Next to", "3) Between", "4) In Front of"]);
+questions.push(q);
+
+q = test.create_question("Ф","4");
+test.set_multichoice(q, ["1) Behind", "2) Next to", "3) Between", "4) In Front of"]);
+questions.push(q);
+
+test.start_test(questions);
